9d9eade75946f5fa82d574ec4eeef877ae78222d
dudududu144 <dudududi144@gmail.com>
2026-08-14 09:28:55 +0300

    Update __init__.py: add new modules

--- acid/__init__.py
+++ acid/__init__.py
@@ -1,1 +1,34 @@
-"""ACID - Autonomous Computational Intelligence Discovery"""
+"""
+ACID - Autonomous Computational Intelligence Discovery
+
+A system that discovers, verifies, distills, and transfers
+computational mechanisms through evolutionary search.
+"""
+
+__version__ = "4.0.0"
+
+from acid.substrate import Program, Executor, PRIMITIVES
+from acid.search import DiscoveryEngine, random_program, mutate_program
+from acid.verifier import VerificationLayer
+from acid.distiller import DistillationPipeline
+from acid.transfer import TransferEngine
+from acid.evidence import EvidenceGraph
+from acid.adversary import Adversary
+from acid.improver import SelfImprovementMeasurer
+from acid.knowledge_graph import KnowledgeGraph, build_graph_from_history
+from acid.experiment import Experiment, AblationStudy, StatisticalTest
+from acid.feedback import FeedbackLoop
+
+__all__ = [
+    "Program", "Executor", "PRIMITIVES",
+    "DiscoveryEngine", "random_program", "mutate_program",
+    "VerificationLayer",
+    "DistillationPipeline",
+    "TransferEngine",
+    "EvidenceGraph",
+    "Adversary",
+    "SelfImprovementMeasurer",
+    "KnowledgeGraph", "build_graph_from_history",
+    "Experiment", "AblationStudy", "StatisticalTest",
+    "FeedbackLoop",
+]
